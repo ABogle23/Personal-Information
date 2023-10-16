d3b7bf68ae3daeb33e251872fb2cfdb8638d78ed
Edward White <edward.white23@imperial.ac.uk>
2023-10-16 22:18:11 +0100

    Correcting python syntax

--- api/app.py
+++ api/app.py
@@ -20,6 +20,6 @@ def submit():
             bicycle_recommendation = "Hybrid Bike"
         else:
             bicycle_recommendation = "Road Bike"
-    elif surface = "Off-Road":
+    elif surface == "Off-Road":
         bicycle_recommendation == "Mountain Bike"
     return render_template("hello.html", name=input_name, age=input_age, surface=surface, activity=activity, storage=storage, bicycle_recommendation=bicycle_recommendation)
