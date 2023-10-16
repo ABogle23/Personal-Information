8402dd7ccd733aaeb7c8f887db387237a5d31f50
Edward White <edward.white23@imperial.ac.uk>
2023-10-16 22:14:28 +0100

    Attempting to correct spacing errors caused by vim

--- api/app.py
+++ api/app.py
@@ -9,20 +9,17 @@ def hello_world():
 def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
-	surface = request.form.get("surface")
-	activity = request.form.get("activity")
-	storage = request.form.get("storage")
-
-	bicycle_recommendation = ""
-
-	if surface == "Road":
-		if storage == "Limited":
-			bicycle_recommendation = "Folding Bike"
-		elif activity == "Commuting":
-			bicycle_recommendation = "Hybrid Bike"
-		else:
-			bicycle_recommendation = "Road Bike"
-	elif surface = "Off-Road":
-		bicycle_recommendation == "Mountain Bike"
-
+    surface = request.form.get("surface")
+    activity = request.form.get("activity")
+    storage = request.form.get("storage")
+    bicycle_recommendation = ""
+    if surface == "Road":
+        if storage == "Limited":
+            bicycle_recommendation = "Folding Bike"
+        elif activity == "Commuting":
+            bicycle_recommendation = "Hybrid Bike"
+        else:
+            bicycle_recommendation = "Road Bike"
+    elif surface = "Off-Road":
+        bicycle_recommendation == "Mountain Bike"
     return render_template("hello.html", name=input_name, age=input_age, surface=surface, activity=activity, storage=storage, bicycle_recommendation=bicycle_recommendation)
