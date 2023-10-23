0e6a9a07dbc8bb42abe4c07943c0e1d139990d2b
Lucian Mac-Fall <ltm23@ic.ac.uk>
2023-10-23 16:04:55 +0100

    check to see successful auto build

--- api/app.py
+++ api/app.py
@@ -23,7 +23,7 @@ def submit():
         else:
             bicycle_recommendation = "Road Bike"
     else:
-        bicycle_recommendation = "Mountain Bike"
+        bicycle_recommendation = "Mountain Bike lol"
     return render_template(
         "hello.html",
         name=input_name,
